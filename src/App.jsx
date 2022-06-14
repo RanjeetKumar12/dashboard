2cd66f1e715304e26d61aea2dc5227181699e8ba
pokir <pokir@users.noreply.github.com>
2022-06-14 21:43:23 +0300

    Add title

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import DiscordLogin from './components/DiscordLogin';
 const App = () => {
   return (
     <div className="App">
+      <h1>Welcome to Graveyard Dashboard! Please login to use it.</h1>
+
       <DiscordLogin />
     </div>
   );
