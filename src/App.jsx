339c726c78cb2c6ce3abc2d157d6247421081c3c
pokir <pokir@users.noreply.github.com>
2022-06-19 22:58:31 +0300

    Don't show the login button if user is logged in

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import DashboardWrapper from './components/DashboardWrapper';
 const App = () => {
   const [ loggedIn, setLoggedIn ] = useState(false);
 
+  const onLoginCallback = useCallback(() => {
+    setLoggedIn(true);
+  }, []);
+
   return (
     <div className="App">
-      <h1>Welcome to Graveyard Dashboard! Please login to use it.</h1>
+      <h1>Welcome to Graveyard Dashboard!</h1>
 
-      <DiscordLogin
-        onLogin={useCallback(() => {
-          setLoggedIn(true);
-        }, [])}
-      />
+      {!loggedIn && <DiscordLogin onLogin={onLoginCallback} />}
 
       {loggedIn && <DashboardWrapper />}
     </div>
